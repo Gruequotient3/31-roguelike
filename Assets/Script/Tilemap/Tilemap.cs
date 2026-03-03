fba81d2338ea7d46434e3b88631f2116811babfe
LEVASTRE Clément <levastre.clement@gmail.com>
2026-03-03 09:30:57 +0100

    Remove old Tile class definition

--- Assets/Script/Tilemap/Tilemap.cs
+++ Assets/Script/Tilemap/Tilemap.cs
@@ -5,46 +5,6 @@ using UnityEngine;
 using Roguelike.Utils;
 
 namespace Roguelike.Tilemaps{
-    [Serializable]
-    public struct TileData
-    {
-        public Sprite sprite;
-        public bool seleted;
-    }
-
-    public class Tile
-    {
-        public GameObject gameObject = null;
-
-        private Vector3Int _position;
-        private TileData _tileData;
-
-        public Tile(Vector3Int position, TileData tileData){
-            _position = position;
-            _tileData = tileData;
-        }
-
-        public void Generate()
-        {
-            if (gameObject) UnityEngine.Object.Destroy(gameObject);
-            gameObject = new GameObject("Tile " + _position.x + " " + _position.y + " " + _position.z);
-            SpriteRenderer sp = gameObject.AddComponent<SpriteRenderer>();
-            sp.sprite = _tileData.sprite;
-
-            Vector3 pos = Coordinate.IsoToWorld(_position);
-
-            sp.sortingOrder = -(_position.x + _position.y) + _position.z * 5;
-            gameObject.transform.position = new Vector3(pos.x, pos.y, 0.0f);
-        }
-
-        public Sprite GetSprite(){ return _tileData.sprite; }
-        public void SetSprite(Sprite sprite)
-        {
-            _tileData.sprite = sprite;
-            gameObject.GetComponent<SpriteRenderer>().sprite = sprite;
-        }
-    }
-
     public class Tilemap
     {
         public GameObject gameObject = null;
